3734010088209dfc4b5bf5a32498820e3bd41994
Rima Jean <rima.jean@gmail.com>
2018-11-10 17:26:44 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,7 +2,6 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-
   def self.table_name
     self.to_s.downcase.pluralize
   end
@@ -13,14 +12,14 @@ class InteractiveRecord
     sql = "PRAGMA table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
-   column_names = []
-   table_info.each do |row|
-     column_names << row["name"]
-   end
-   column_names.compact
- end
+    column_names = []
+    table_info.each do |row|
+      column_names << row["name"]
+    end
+    column_names.compact
+  end
 
- def initialize(options={})
+  def initialize(options={})
     options.each do |property, value|
       self.send("#{property}=", value)
     end
